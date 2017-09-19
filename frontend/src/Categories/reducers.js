c6cde3ca5b1ca4f4aee3100f75dcadb38f48c556
Phil Gates-Shannon <phil.gs@gmail.com>
2017-09-19 07:15:20 -0400

    Add reducer for downloading category list

--- frontend/src/Categories/reducers.js
+++ frontend/src/Categories/reducers.js
@@ -2,11 +2,8 @@ import { DOWNLOAD_CATEGORIES_COMPLETE } from '../constants';
 
 const categoriesReducer = function( state={}, action ) {
     switch( action.type ) {
-        // case DOWNLOAD_CATEGORIES_COMPLETE:
-        //     return {
-        //         ...state,
-        //         categories: action.data.categories
-        //     };
+        case DOWNLOAD_CATEGORIES_COMPLETE:
+            return action.data.categories;
         default:
             return state;
     }
