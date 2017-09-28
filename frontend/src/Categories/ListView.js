318024c735948854331b17d9ab10134a2870eb2b
Phil Gates-Shannon <phil.gs@gmail.com>
2017-09-28 06:57:35 -0400

    Connect ListView component to store, so it gets post data

--- frontend/src/Categories/ListView.js
+++ frontend/src/Categories/ListView.js
@@ -1,11 +1,27 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 import { connect } from 'react-redux';
 import { downloadPostsStart } from '../Posts/actions';
+import { STORE_POSTS_BY_CATEGORY, STORE_POSTS_DATA } from '../constants';
 
+// TODO Does this update correctly as a `PureComponent`?
 class ListView extends Component {
     static propTypes = {
-        category: PropTypes.string.isRequired
+        category: PropTypes.string.isRequired,
+        [STORE_POSTS_BY_CATEGORY]: ImmutablePropTypes.mapOf(
+            ImmutablePropTypes.setOf( PropTypes.string )
+        ).isRequired,
+        [STORE_POSTS_DATA]: ImmutablePropTypes.mapContains( {
+            author: PropTypes.string,
+            body: PropTypes.string,
+            category: PropTypes.string,
+            deleted: PropTypes.bool,
+            id: PropTypes.string,
+            timestamp: PropTypes.number,
+            title: PropTypes.string,
+            voteScore: PropTypes.number
+        } ).isRequired
     };
 
     componentDidMount() {
@@ -18,4 +34,7 @@ class ListView extends Component {
 
 }
 
-export default connect(  state => ({ posts: state.posts }) )( ListView );
+export default connect( state => ({
+    [STORE_POSTS_BY_CATEGORY]: state.get( STORE_POSTS_BY_CATEGORY ),
+    [STORE_POSTS_DATA]: state.get( STORE_POSTS_DATA )
+}) )( ListView );
