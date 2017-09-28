204f804d83ad854c45250831381cd7716e3311dd
Phil Gates-Shannon <phil.gs@gmail.com>
2017-09-28 06:19:39 -0400

    Implement posts-by-category reducer

--- frontend/src/Posts/reducers.js
+++ frontend/src/Posts/reducers.js
@@ -1,14 +1,25 @@
 import Immutable from 'immutable';
 import { DOWNLOAD_POSTS_COMPLETE } from '../constants';
 
+const postsByCategoryDefaultState = Immutable.Map();
 const postsDataDefaultState = Immutable.List();
-const postsByCategoryDefaultState = Immutable.List();
 
 export const postsByCategoryReducer = function( state=postsByCategoryDefaultState, action ) {
     switch( action.type ) {
         case DOWNLOAD_POSTS_COMPLETE:
-            // TODO process a list of posts
-            return action.data.posts ? Immutable.fromJS( action.data.posts ) : postsDataDefaultState;
+            return state.withMutations( mutableState => {
+                action.data.posts.forEach( post => {
+                    // TODO Use arrays or mutable Sets to improve performance
+                    let categoryPosts = null;
+                    if ( mutableState.has( post.category ) ) {
+                        categoryPosts = mutableState.get( post.category );
+                    } else {
+                        categoryPosts = Immutable.Set();
+                    }
+                    categoryPosts = categoryPosts.add( post.id );
+                    mutableState.set( post.category, categoryPosts );
+                } );
+            } );
         default:
             return state;
     }
